1b508a0bf5d40507955ea69c5cf14e8cf4582f94
Abbas <abbas@unmarshal.io>
2023-01-09 18:50:45 +0530

    fix: code fixes that were resulting in error

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,27 +1,23 @@
-import {near, BigInt} from "@graphprotocol/graph-ts"
-import {Block, BlockDetails} from "../generated/schema";
+import { near, BigInt } from "@graphprotocol/graph-ts";
+import { Block, BlockDetails } from "../generated/schema";
 
-export function handleNewBlock(
-	block: near.Block
-): void {
-	console.log(block.header.hash.toString())
-	let newBlock = new Block(block.header.hash.toString())
+export function handleBlock(block: near.Block): void {
+	const header = block.header;
+	let event = new Block(header.hash.toHexString());
+	event.blockNumber = BigInt.fromI32(header.height as i32);
+	event.timestamp = BigInt.fromU64(header.timestampNanosec);
 
-	newBlock.blockNumber = block.header.height as i32;
-	newBlock.timestamp = block.header.timestampNanosec as i32;
-
-	let blockDetails = new BlockDetails(block.header.hash.toString())
-
+	let detailsEvent = new BlockDetails(header.hash.toHexString())
 	let chunkHashes:Array<string> = [];
 	let chunkIds:i32[] = [];
 	for (let i = 0; i < block.chunks.length; i++) {
-		chunkHashes.push(block.chunks[i].chunkHash.toString())
+		chunkHashes.push(block.chunks[i].chunkHash.toHexString())
 		chunkIds.push(block.chunks[i].shardId as i32)
 	}
-	blockDetails.chunkHashes = chunkHashes
-	blockDetails.chunkIds = chunkIds
-	blockDetails.save();
+	detailsEvent.chunkHashes = chunkHashes
+	detailsEvent.chunkIds = chunkIds
+	detailsEvent.save();
 
-	newBlock.blockDetails = blockDetails.id;
-	newBlock.save();
+	event.blockDetails = detailsEvent.id
+	event.save();
 }
